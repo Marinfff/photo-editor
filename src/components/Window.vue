8fe12c90f64df152c864684c83640110fe8c6cd8
Aurel <aurelgandrabura@Gmail.com>
2020-01-14 10:50:19 +0200

    refacotor

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,22 +1,36 @@
 <script>
+    import fs from 'fs'
+    const {dialog} = require('electron').remote;
 
-export default {
-  name: 'Window',
-  data () {
-    return {
-      name: 'puişor'
-    }
-  },
-  mounted() {},
-  methods: {}
-};
+    export default {
+        name: 'Window',
+        data () {
+            return {
+                expression: '',
+                result: '',
+                path: './src/files/test.txt'
+            }
+        },
+        methods: {
+            readFromTxt(path) {
+                fs.readFile(path, "utf8", (error, data) => {
+                    this.expression = data
+                });
+            },
+            evaluate () {
+                console.log(this.expression, eval(this.expression))
+                this.result = eval(this.expression)
+            }
+        }
+    };
 </script>
 
 <template>
-  <div>
-    <div>Hello {{name}}!</div>
-    <v-btn color="red">Test</v-btn>
-  </div>
+    <div>
+        <div>{{expression}} = {{result}}</div>
+        <v-btn @click="readFromTxt(path)" color="blue">Read from text</v-btn>
+        <v-btn @click="evaluate()" color="green">Eval</v-btn>
+    </div>
 </template>
 
 <style scoped>
